69579e1b99807b194cec30bc59520991f32a1484
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-15 17:12:59 -0700

    complete array find lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+  const winningRecord = record.find(lookAtRecord);
+  if (winningRecord) {
+    return winningRecord.year;
+  }
+}
+
+function lookAtRecord(element) {
+  return element.result === "W";
+}
